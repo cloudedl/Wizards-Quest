bb83de15e62a58fe194c67c44305c88bc74e4cd7
cloud3999 <edwardlcloud@gmail.com>
2022-02-18 16:47:31 -0500

    Setup initial links and pseudo code

---
+++ js/app.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", () => {})
+
+//Create Static Splash Screen in HTML
+    //Create a button that manipulates the DOM to load the first fight
+    //Bonus: Add theme music
+
+
+//Manipulate the DOM to Create Elements
+    // 4 buttons 3 attack spells, 1 heal
+    //place player and enemy locations
+    //enemy elemental will need stats stored in an object
+    //each spell will be stored in an object as well. 
+    //build status bars at the top and bottom of the screen
+    //draw a box around everything.
+
+//have each attack button damage read the Element Type of the Enemy and do damage based off the element type.
+    //Bonus: have a temporary dialog box appear saying "super effective" or "not very effective"
+//heal button will raise the player's health by a ranged amount
+//create enemy AI, enemy will attack a few seconds after the player plays their turn. Enemy attack will do a range of damage.
+
+//if the Elementals hp reaches zero then the battle is over. Ability buttons can no longer be clicked. Victory screen appears for a few seconds
+// if player is defeated then Defeat Screen Appears with a button to restart: restart takes you back to the Splash Screen
