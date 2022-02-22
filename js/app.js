1e71126166e1902ff3bb270b4215c14a077944d9
cloud3999 <edwardlcloud@gmail.com>
2022-02-22 10:18:34 -0500

    created damage functions for battler

--- js/app.js
+++ js/app.js
@@ -1,4 +1,108 @@
-document.addEventListener("DOMContentLoaded", () => {})
+// document.addEventListener("DOMContentLoaded", () => {})
+
+
+
+//Objects for Characters
+    const fireElemental = {
+        name: 'Fire Elemental',
+        type: 'fire',
+        health: 40
+    }
+    console.log('fire elemental object before damage', fireElemental)
+    
+    const waterElemental = {
+        name: 'Water Elemental',
+        type: 'water',
+        health: 40
+    }
+
+    const grassElemental = { 
+        name: 'Grass Elemental',
+        type: 'grass',
+        health: 40
+    }
+    const wizard = {
+        name: 'wizard',
+        health: 40
+    }
+
+
+
+// Functions for Attacks and Heals
+    const fireball = () => {
+        min = Math.ceil(1);
+        max = Math.floor(8);
+        let damage = Math.floor(Math.random() * (8 - 1) + 1);
+        console.log('damage done by fireball', damage)
+        // fireElemental.health = fireElemental.health - damage
+        if (fireElemental.type === 'grass') {
+            damage = 1.5 * damage
+            fireElemental.health = fireElemental.health - damage
+        } else if (fireElemental.type === 'water') {
+            damage = damage / 2
+            fireElemental.health = fireElemental.health - damage
+        } else {
+            fireElemental.health = fireElemental.health + damage
+        } 
+        
+    }
+    // console.log('is my fireball producing damage', fireball())
+    fireball()
+    console.log('did fireball reduce health of fireElemental' , fireElemental)
+
+    const waterJet = () => {
+        min = Math.ceil(1);
+        max = Math.floor(8);
+        let damage = Math.floor(Math.random() * (8 - 1) + 1);
+
+        if (enemy.type === 'fire') {
+            damage = 1.5 * damage
+            return damage
+        } else if (enemy.type === 'grass') {
+            damage = damage / 2
+            return damage
+        } else {
+            return damage
+        }
+    }
+
+    const vineStrike = () => {
+        min = Math.ceil(1);
+        max = Math.floor(8);
+        let damage = Math.floor(Math.random() * (8 - 1) + 1);
+
+        if (enemy.type === 'water') {
+            damage = 1.5 * damage
+            return damage
+        } else if (enemy.type === 'fire') {
+            damage = damage / 2
+            return damage
+        } else {
+            return damage
+        }
+    }
+
+    const heal = () => {
+        min = Math.ceil(2);
+        max = Math.floor(12);
+        return Math.floor(Math.random() * (12 - 2) + 2);
+    }
+    // console.log('does heal do 2 - 12 randomly?' , heal())
+
+    const enemyAttack = () => {
+        min = Math.ceil(1);
+        max = Math.floor(8);
+        let damage = Math.floor(Math.random() * (8 - 1) + 1);
+
+    }
+
+    const enemySuper =() => {
+        min = Math.ceil(5);
+        max = Math.floor(15);
+        let damage = Math.floor(Math.random() * (8 - 1) + 1);
+
+    }
+
 
 //Create Static Splash Screen in HTML
     //Create a button that manipulates the DOM to load the first fight
@@ -9,7 +113,6 @@ document.addEventListener("DOMContentLoaded", () => {})
     // 4 buttons 3 attack spells, 1 heal
     //place player and enemy locations
     //enemy elemental will need stats stored in an object
-    //each spell will be stored in an object as well. 
     //build status bars at the top and bottom of the screen
     //draw a box around everything.
 
